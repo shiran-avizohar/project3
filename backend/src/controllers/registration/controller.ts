67c6bce1689436200d26e79446200e4a115f203a
shiran avizohar <shiran0204@gmail.com>
2025-03-26 22:55:06 +0200

    new controller - register in backend

--- backend/src/controllers/registration/controller.ts
+++ backend/src/controllers/registration/controller.ts
@@ -1,32 +1,33 @@
 import { Request, Response } from "express";
 import User from "../../models/user";
 import bcrypt from "bcryptjs";
-
-
-// פונקציה לרישום משתמש חדש
+//func to register new user
 export const register = async (req: Request, res: Response) => {
-    try {
-        const { name, email, password } = req.body;
-
-        // בדיקה אם המשתמש כבר קיים
-        const existingUser = await User.findOne({ where: { email } });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        // הצפנת הסיסמה
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // יצירת המשתמש ושמירתו בדאטהבייס
-        const newUser = await User.create({
-            name,
-            email,
-            password: hashedPassword,
-        });
-
-        return res.status(201).json({ message: "User registered successfully", user: newUser });
-    } catch (error) {
-        console.error("Registration error:", error);
-        return res.status(500).json({ message: "Internal server error" });
+  try {
+    const { firstName, lastName, role, email, password } = req.body;
+    console.log(req.body);
+    // // בדיקה אם המשתמש כבר קיים
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
     }
+
+    // הצפנת הסיסמה
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // יצירת המשתמש ושמירתו בדאטהבייס
+    const newUser = await User.create({
+      firstName: firstName,
+      lastName: lastName,
+      role: role,
+      email: email,
+      password: hashedPassword,
+    });
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
