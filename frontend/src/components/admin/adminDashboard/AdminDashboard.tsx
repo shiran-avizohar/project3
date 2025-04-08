b3740c4a298c88e37ee3af144b9e593557204341
shiran avizohar <shiran0204@gmail.com>
2025-04-08 13:00:02 +0300

    adding links to admin dashboard

--- frontend/src/components/admin/adminDashboard/AdminDashboard.tsx
+++ frontend/src/components/admin/adminDashboard/AdminDashboard.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import './AdminDashboard.css'
+import './AdminDashboard.css';
 
 const Dashboard = () => {
   return (
@@ -7,12 +7,20 @@ const Dashboard = () => {
       <h2 className="dashboard-header">Admin Dashboard</h2>
       
       <div className="dashboard-links">
-        <Link to="/admin/add">
+        <Link to="/admin/addVacation">
           <button className="dashboard-button">Add New Vacation</button>
         </Link>
         
-        <Link to="/admin/view">
-          <button className="dashboard-button">View All Vacations</button>
+        <Link to="/admin/manageVacations">
+          <button className="dashboard-button">Manage Vacations</button>
+        </Link>
+
+        <Link to="/admin/reports">
+          <button className="dashboard-button">Reports</button>
+        </Link>
+        
+        <Link to="/admin/csvDownload">
+          <button className="dashboard-button">Download Reports</button>
         </Link>
       </div>
     </div>
