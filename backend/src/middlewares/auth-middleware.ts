c040059738be87fe2780fd4a7ad12ee38e4b7781
shiran avizohar <shiran0204@gmail.com>
2025-04-18 16:36:46 +0300

    comment delete

--- backend/src/middlewares/auth-middleware.ts
+++ backend/src/middlewares/auth-middleware.ts
@@ -16,7 +16,7 @@ export function authenticateUser(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // ⬅️ זה קריטי
+        req.user = decoded; 
         next();
     } catch (err) {
         return res.status(401).json({ message: "Invalid token" });
