d374706d8b09b922107c7036d923f7ff0b29e538
shiran avizohar <shiran0204@gmail.com>
2025-04-20 13:01:26 +0300

    delete comments sequelize

--- backend/src/db/sequelize.ts
+++ backend/src/db/sequelize.ts
@@ -1,28 +1,10 @@
-// import { Sequelize } from "sequelize-typescript";
-// import config from 'config'
-// import Vacation from "../models/vacation";
-// import Follow from "../models/follow";
-// import User from "../models/user";
-
-// const logging = config.get<boolean>('sequelize.logging') ? console.log : false
-
-// const sequelize = new Sequelize({
-//     // [ add ALL model classes you created to the array ]:
-//     models: [ User, Vacation, Follow ],
-//     dialect: 'mysql',
-//     ...config.get('db'),
-//     logging: console.log,
-// })
-
-// export default sequelize
-
 import { Sequelize } from "sequelize-typescript";
 import config from "config";
 import Vacation from "../models/vacation";
 import Follow from "../models/follow";
 import User from "../models/user";
 
-// הגדרת פונקציה עבור חיבור ל-MySQL עם config
+
 const logging = config.get<boolean>('sequelize.logging') ? console.log : false;
 
 const sequelize = new Sequelize({
