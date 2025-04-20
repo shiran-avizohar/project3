3ee946073d7689d1fa5c136468c5d84e5a94db92
shiran avizohar <shiran0204@gmail.com>
2025-04-20 05:29:09 +0300

    adding user name and style css to user dashboard

--- frontend/src/components/user/userDashboard/UserDashboard.tsx
+++ frontend/src/components/user/userDashboard/UserDashboard.tsx
@@ -7,23 +7,21 @@ const UserDashboard = () => {
   const user = JSON.parse(localStorage.getItem("user") || "{}");
 
   useEffect(() => {
-    // If the user is not logged in (no email in localStorage), navigate to the login page
-    if (!user?.user?.email) {
+    if (!user?.email) {
       navigate("/login");
     }
-  }, [navigate, user?.user]);
+  }, [navigate, user?.email]);
 
   return (
     <div className="user-dashboard-container">
       <h2 className="user-dashboard-header">Welcome to DreamTrip</h2>
       <div className="user-dashboard-info">
-        <h3>Hey {user?.name || "Guest"}!</h3>
+        <h3>Hey {user?.firstName || "Guest"}!</h3>
         <p>We're so glad you're here! Get ready to plan your dream vacation with us.</p>
         <p>Explore, discover, and make your perfect getaway a reality!</p>
       </div>
       <div className="user-dashboard-links">
-        {/* Link to the 'Vacations' page */}
-        <Link to="/user/vacations"><button className="user-dashboard-button">My Vacations</button></Link>
+        <Link to="/user/vacations"><button className="user-dashboard-button"> Find Your Dream Trip</button></Link>
       </div>
     </div>
   );
