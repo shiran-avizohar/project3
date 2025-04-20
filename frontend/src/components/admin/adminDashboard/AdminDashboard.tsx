184c22f8b2e783107d67e34070cc168e612ae453
shiran avizohar <shiran0204@gmail.com>
2025-04-20 06:41:30 +0300

    change words

--- frontend/src/components/admin/adminDashboard/AdminDashboard.tsx
+++ frontend/src/components/admin/adminDashboard/AdminDashboard.tsx
@@ -1,20 +1,20 @@
-import { Link } from 'react-router-dom';
-import './AdminDashboard.css';
+import { Link } from "react-router-dom";
+import "./AdminDashboard.css";
 
 const Dashboard = () => {
   return (
-<div className="dashboard-container">
+    <div className="dashboard-container">
       <h2 className="dashboard-header">
-        Hello! <br></br>
-        What would you like to do today?
+        Welcome, Admin.
+        <br />
+        What would you like to manage today?
       </h2>
-
 
       <div className="dashboard-links">
         <Link to="/admin/addVacation">
           <button className="dashboard-button">Add New Vacation</button>
         </Link>
-        
+
         <Link to="/admin/VacationManagement">
           <button className="dashboard-button">Vacation Management</button>
         </Link>
@@ -22,7 +22,6 @@ const Dashboard = () => {
         <Link to="/admin/reports">
           <button className="dashboard-button"> View Reports</button>
         </Link>
-        
       </div>
     </div>
   );
